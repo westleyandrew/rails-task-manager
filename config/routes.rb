4c692de2ad910b02c7b476be737cec9395449d19
Westley Andrew <westleyandrew@hotmail.com>
2019-08-13 14:59:43 +0100

    basics done

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
   get 'tasks/:id', to: 'tasks#show', as: :task
-  delete 'tasks/:id', to: 'restaurants#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
